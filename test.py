f65e99d49b671fd511e4392f93d931a50b66f2a9
Doraasiro <dorasiro1@live.jp>
2026-02-01 12:43:37 +0900

    ユーザープロンプトを変更できることを確認

--- test.py
+++ test.py
@@ -17,8 +17,17 @@ class ChatRequest(BaseModel):
     messages: list[Message]
     stream: bool = False
 
+# モデル一覧を取得　OllamaサーバーのOpenAI-API互換機能を使って取得したものをそのまま流すだけ
+@app.get("/v1/models")
+def list_models():
+    r = requests.get("http://" + PRIVATE_IP + ":11434/v1/models")
+    return r.json()
+
 @app.post("/v1/chat/completions")
 def chat_completions(req: ChatRequest):
+    # ユーザープロンプトをオーバーライドしてみる
+    #req.messages[len(req.messages)-1].content = "ピサの斜塔について解説して"
+
     ollama_payload = {
         "model": req.model,
         "messages": [{"role": m.role, "content": m.content} for m in req.messages],
